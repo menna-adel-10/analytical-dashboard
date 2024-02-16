60ecabbab15a0d619d922301434938bca4a46d55
menna-adel-10 <menna10_adel@hotmail.com>
2024-02-16 17:46:20 +0200

    .

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -2,15 +2,54 @@
 
 import { analytics } from '@/utils/analytics'
 import { BarChart, Card } from '@tremor/react'
+import { ArrowRight, ArrowUpRight, ArrowDownRight } from 'lucide-react'
 import React from 'react'
+import ReactCountryFlag from 'react-country-flag'
 
 interface AnalyticsDashboardProps {
     avgVisitorsPerDay: string
     amtVisitorsToday: number
     timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>
+    topCountries: [string, number][]
+
 }
 
-const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews }:
+const Badge = ({ percentage }: { percentage: number }) => {
+    const isPositive = percentage > 0
+    const isNeutral = percentage === 0
+    const isNegative = percentage < 0
+
+    if (isNaN(percentage)) return null
+
+    const positiveClassname = `bg-green-900/25 text-green-400 ring-green-400/25`
+    const neutralClassname = `bg-zinc-900/25 text-zinc-400 ring-zinc-400/25`
+    const negativeClassname = `bg-red-900/25 text-red-400 ring-red-400/25`
+
+    return (
+        <span className={`inline-flex gap-1 items-center rounded-md px-2 py-1 
+        text-xs font-medium ring-1 ring-inset ${
+        isPositive
+            ? positiveClassname
+            : isNeutral
+            ? neutralClassname
+            : negativeClassname
+            
+        }`
+        }>
+            {isPositive ? <ArrowUpRight className="h-3 w-3" /> : null}
+            {isNeutral ? <ArrowRight className="h-3 w-3" /> : null}
+            {isNegative ? <ArrowDownRight className="h-3 w-3" /> : null}
+            {percentage.toFixed(0)}%
+        </span>
+    )
+};
+
+const AnalyticsDashboard = ({
+    avgVisitorsPerDay,
+    amtVisitorsToday,
+    timeseriesPageviews,
+    topCountries
+}:
     AnalyticsDashboardProps) => {
   return (
       <div className="flex flex-col gap-6">
@@ -25,8 +64,13 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday, timeseriesPag
 
               </Card>
               <Card className="w-full mx-auto max-w-xs">
-                  <p className="text-tremor-default text-dark-tremor-content">
+                  <p className="flex gap-2.5 items-center text-tremor-default text-dark-tremor-content">
                       Visitors today
+                      <Badge
+                          percentage={
+                              (amtVisitorsToday / Number(avgVisitorsPerDay) - 1) * 100
+                      }
+                      />
                   </p>
                   <p className="text-3xl text-dark-tremor-content-strong font-semibold">
                       {amtVisitorsToday}
@@ -34,6 +78,29 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday, timeseriesPag
 
               </Card>
           </div>
+
+          <Card className="flex flex-col sm:grid grid-cols-4 gap-6">
+              <h2 className="w-full text-dark-tremor-content-strong text-center sm:left-left font-semibold text-xl">
+                  This weeks top visitors:
+              </h2>
+              <div className="col-span-3 flex items-center justify-between flex-wrap gap-8">
+                  {topCountries?.map(([countryCode, number]) => {
+                      return <div  key={countryCode} className="flex items-center gap-3 text-dark-tremor-content-strong">
+                          <p className="hidden sm:block text-tremor-content">
+                              {countryCode}
+                          </p>
+                          <ReactCountryFlag
+                             
+                              className="text-5xl sm:text-3xl"
+                              svg
+                              countryCode={countryCode}
+                          />
+
+                          <p className="text-tremor-content sm:text-dark-tremor-content-strong">{number}</p>
+                      </div>
+                  })}
+              </div>
+          </Card>
 
           <Card>
               {timeseriesPageviews ? (
