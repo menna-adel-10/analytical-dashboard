7e4334a89ec89ceb02ac0017003cc3e1b07864b2
menna-adel-10 <menna10_adel@hotmail.com>
2024-02-16 13:40:17 +0200

    .

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,4 +1,5 @@
 import AnalyticsDashboard from '@/components/AnalyticsDashboard'
+import { getData } from '@/utils'
 import { analytics } from '@/utils/analytics'
 import React from 'react'
 
@@ -16,10 +17,38 @@ const Page = async () => {
     }, 0)
 
     const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1)
+
+    const amtVisitorsToday = pageviews
+        .filter((ev) => ev.date === getData())
+        .reduce((acc, curr) => {
+          return (
+            acc + curr.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!
+            }, 0)
+        )
+        }, 0)
+    
+    const topCountries = new Map<string, number>()
+
+    for (let i = 0; i < pageviews.length; i++) {
+        const day = pageviews[i];
+        if (!day) continue
+        
+        for (let j = 0; j < day.events.length; j++) {
+            const event = day.events[j];
+            if (!event) continue
+            
+        }
+        
+    }
   return (
       <div className="min-h-screen w-full py-12 flex justify-center items-center">
           <div className="relative w-full max-w-6xl mx-auto text-white">
-              <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
+              <AnalyticsDashboard
+                  avgVisitorsPerDay={avgVisitorsPerDay}
+                  amtVisitorsToday={amtVisitorsToday}
+                  timeseriesPageviews={pageviews}
+              />
           </div>
     </div>
   )
--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { Card } from '@tremor/react'
+import { analytics } from '@/utils/analytics'
+import { BarChart, Card } from '@tremor/react'
 import React from 'react'
 
 interface AnalyticsDashboardProps {
     avgVisitorsPerDay: string
+    amtVisitorsToday: number
+    timeseriesPageviews: Awaited<ReturnType<typeof analytics.retrieveDays>>
 }
 
-const AnalyticsDashboard = ({ avgVisitorsPerDay }:
+const AnalyticsDashboard = ({ avgVisitorsPerDay, amtVisitorsToday, timeseriesPageviews }:
     AnalyticsDashboardProps) => {
   return (
       <div className="flex flex-col gap-6">
@@ -21,7 +24,33 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay }:
                   </p>
 
               </Card>
+              <Card className="w-full mx-auto max-w-xs">
+                  <p className="text-tremor-default text-dark-tremor-content">
+                      Visitors today
+                  </p>
+                  <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+                      {amtVisitorsToday}
+                  </p>
+
+              </Card>
           </div>
+
+          <Card>
+              {timeseriesPageviews ? (
+                  <BarChart
+                      allowDecimals={false}
+                      showAnimation
+                      data={timeseriesPageviews.map((day) => ({
+                          name: day.date,
+                          Visitors: day.events.reduce((acc, curr) => {
+                              return acc + Object.values(curr)[0]!
+                          }, 0),
+                      }))}
+                      categories={["Visitors"]}
+                      index='name'
+                  />
+              ) : null}
+          </Card>
     </div>
   )
 }
